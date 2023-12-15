f992c7fc0a0e481699cf93b6212aaeee70b03b38
closzach <constantinlozach@gmail.com>
2023-12-15 19:12:09 +0100

    Conversion de la BD en mysql.

--- apps/Astrolabe/Astrolabe/settings.py
+++ apps/Astrolabe/Astrolabe/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'Astrolabe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # Utilise le moteur MySQL pour MariaDB
+        'NAME': 'astrolabe_admin',  
+        'USER': 'root',  
+        'PASSWORD': 'root', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306',
     }
 }
 
